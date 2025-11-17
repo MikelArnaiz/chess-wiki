2aa88e5824b9a1d73e07fd9d4de8ad239dc6afb8
MikelArnaiz <mikel.arnaiz@gmail.com>
2025-11-17 10:05:42 +0100

    adds react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage/Home'
+import { UserPage } from './pages/UserPage/UserPage'
 
-createRoot(document.getElementById('root')!).render(<StrictMode>_</StrictMode>)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user/:username" element={<UserPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
