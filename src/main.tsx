e694db8b82d5489bbd2af621b1ba53ec9068c4d7
MikelArnaiz <mikel.arnaiz@gmail.com>
2025-11-17 15:47:29 +0100

    adds not found page

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { HomePage } from './pages/HomePage/Home'
 import { UserPage } from './pages/UserPage/UserPage'
 import { Navbar } from './components/Navbar/Navbar'
 import { UsersContextProvider } from './components/UsersContextProvider/UsersContextProvider'
+import { NotFound } from './pages/NotFound/NotFound'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/user/:username" element={<UserPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </UsersContextProvider>
     </BrowserRouter>
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,1 @@
+export const NotFound = () => <div>Not found - 404</div>
